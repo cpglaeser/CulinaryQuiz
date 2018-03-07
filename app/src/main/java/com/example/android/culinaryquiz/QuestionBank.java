ca86822acb353ec6206ce8591b2a37c58ee9e730
cpglaeser <35821187+cpglaeser@users.noreply.github.com>
2018-03-07 11:59:54 -0800

    Update QuestionBank.java

--- app/src/main/java/com/example/android/culinaryquiz/QuestionBank.java
+++ app/src/main/java/com/example/android/culinaryquiz/QuestionBank.java
@@ -1,8 +1,42 @@
-package com.example.android.culinaryquiz;
-
 /**
  * Created by coryglaeser on 2/20/18.
  */
 
-class QuestionBank {
+package com.example.android.culinaryquiz;
+import java.util.ArrayList;
+
+//Array of questions
+
+public class QuestionBank {
+
+    ArrayList<Question> list = new ArrayList<>();
+
+    public QuestionBank() {
+
+        list.add(new Question("How many tablespoons are in one cup?",
+                "4",
+                "8",
+                "16",
+                "32",
+                "c"));
+        list.add(new Question("Which of the following are mirepoix ingredients?",
+                "Carrots",
+                "Celery",
+                "Garlic",
+                "Onion",
+                "22"));
+        list.add(new Question("Which of the following is NOT a classic French Mother Sauce?",
+                "Hollandaise",
+                "Mayonnaise",
+                "Espagnole",
+                "Bechamel",
+                "c"));
+        list.add(new Question("In Celcius, what temperature does water boil at?",
+                "",
+                "",
+                "",
+                "",
+                "100"));
+    }
 }
+
