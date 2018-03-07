ce63dd3def2fbf567429849f34bb4b61209cd725
cpglaeser <35821187+cpglaeser@users.noreply.github.com>
2018-03-07 11:59:26 -0800

    Update Question.java

--- app/src/main/java/com/example/android/culinaryquiz/Question.java
+++ app/src/main/java/com/example/android/culinaryquiz/Question.java
@@ -1,8 +1,21 @@
-package com.example.android.culinaryquiz;
-
 /**
  * Created by coryglaeser on 2/20/18.
  */
 
-class Question {
+package com.example.android.culinaryquiz;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question {
+
+    public Map questionSet = new HashMap<>();
+
+    public Question(String questionText, String answer1, String answer2, String answer3, String answer4, String correctAnswer) {
+        questionSet.put("question", questionText);
+        questionSet.put("a", answer1);
+        questionSet.put("b", answer2);
+        questionSet.put("c", answer3);
+        questionSet.put("d", answer4);
+        questionSet.put("answer", correctAnswer);
+    }
 }
